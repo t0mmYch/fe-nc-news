cae1c37b81681b6342b8e017ed47a953d5974040
Tommy Christ M1 Pro <tommychristm1pro@tommys-mbp.home>
2025-02-19 12:07:52 +0000

    Fixed Folder Structure

--- routers/comments.router.js
+++
@@ -1,6 +0,0 @@
-const commentsRouter = require("express").Router()
-const { deleteCommentByCommentId } = require("../controller")
-
-commentsRouter.delete("/:comment_id", deleteCommentByCommentId)
-
-module.exports = commentsRouter
--- routers/topics.router.js
+++
@@ -1,6 +0,0 @@
-const topicsRouter = require("express").Router()
-const { getTopics } = require("../controller")
-
-topicsRouter.get("/", getTopics)
-
-module.exports = topicsRouter
